7cb9e562af39c6ce5f5214c1898ffa515c9ddb53
Harsh2594 <harsh.sgm13@gmail.com>
2024-10-03 18:25:43 +0530

    Implement Graph using Adjacency matrix

---
+++ graph.py
@@ -0,0 +1,37 @@
+#Undirected graph implementation using Adjacency matrix:
+class Graph:
+  def __init__(self,vno):
+    self.adj_matrix = [[0]*vno for _ in range(vno)]
+    self.vertex_count = vno
+       
+  def add_edge(self,u,v,weight=1):
+    if 0<=u<self.vertex_count and 0<=v<self.vertex_count:
+      self.adj_matrix[u][v]=weight
+      self.adj_matrix[v][u]=weight
+    else:
+      print("invalid vertex")
+
+  def remove_edge(self,u,v):
+    if 0<=u<self.vertex_count and 0<=v<self.vertex_count:
+      self.adj_matrix[u][v]=0
+      self.adj_matrix[v][u]=0
+    else:
+      print("invalid vertex")
+
+  def has_edge(self,u,v):
+    if 0<=u<self.vertex_count and 0<=v<self.vertex_count:
+      return self.adj_matrix[u][v] != 0
+    else:
+      print("invalid vertex")
+
+  def print_adj_matrix(self):
+    for row_list in self.adj_matrix:
+      print(" ".join(map(str,row_list)))
+
+g = Graph(3)
+g.print_adj_matrix()  
+print("--------------")  
+g.add_edge(0,2)
+g.print_adj_matrix()
+
+
